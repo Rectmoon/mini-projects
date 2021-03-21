d2d3efe962842f5711e8553ed39c929f0051c025
zhulingying <15692087320@163.com>
2021-03-21 21:27:02 +0800

    refactor: change event

--- dock/script.js
+++ dock/script.js
@@ -1,18 +1,32 @@
 const dock = document.querySelector('#dock')
+const SCALE = 0.618
 
-const curve = x => Math.exp((-x * x) / 100 / 100) + 1
+dock.querySelectorAll('li').forEach(li => {
+  li.addEventListener('mousemove', e => {
+    const { currentTarget } = e
+    const { width, left } = currentTarget.getBoundingClientRect()
+    const offset = Math.abs(e.clientX - left) / width
+    const prev = li.previousElementSibling
+    const next = li.nextElementSibling
 
-dock.addEventListener('mousemove', e => {
-  e.currentTarget.querySelectorAll('li').forEach(li => {
-    li.style.setProperty(
-      '--scale',
-      curve(e.clientX - li.offsetLeft - li.offsetWidth / 2)
-    )
+    resetScale()
+
+    if (prev) {
+      prev.style.setProperty('--scale', 1 + SCALE * Math.abs(offset - 1))
+    }
+
+    li.style.setProperty('--scale', 1 + SCALE)
+
+    if (next) {
+      next.style.setProperty('--scale', 1 + SCALE * offset)
+    }
   })
 })
 
-dock.addEventListener('mouseleave', e => {
-  e.currentTarget.querySelectorAll('li').forEach(li => {
+dock.addEventListener('mouseleave', resetScale)
+
+function resetScale () {
+  dock.querySelectorAll('li').forEach(li => {
     li.style.setProperty('--scale', 1)
   })
-})
+}
